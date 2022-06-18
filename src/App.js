4a0434045fe02a595c8ee948be378f17834ca517
Prakash15 <prakashkumarsingh483@gmail.com>
2022-06-18 20:21:07 +0530

    added navbar and created page comonents

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
 import React from 'react';
 // eslint-disable-next-line import/no-unresolved
 import { Switch, Route } from 'react-router-dom';
+import Navs from './components/Navs';
+import Home from './pages/Home';
+import Starred from './pages/Starred';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        prakash
-      </Route>
-      <Route exact path="/starred">
-        this is starred
-      </Route>
-      <Route> This is 404 Page </Route>
-    </Switch>
+    <div>
+      <Navs />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/starred">
+          <Starred />
+        </Route>
+
+        <Route> Page Not Found </Route>
+      </Switch>
+    </div>
   );
 }
 
