d1bf1db21d696c91a5f376609dc6a12f565f138f
Prakash15 <prakashkumarsingh483@gmail.com>
2022-06-18 19:50:02 +0530

    Added react-router-dom & pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
+// eslint-disable-next-line import/no-unresolved
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Switch>
+      <Route exact path="/">
+        prakash
+      </Route>
+      <Route exact path="/starred">
+        this is starred
+      </Route>
+      <Route> This is 404 Page </Route>
+    </Switch>
+  );
 }
 
 export default App;
