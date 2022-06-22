ec62747a2768cb5091c3ab07c82d0e1e93a23459
Prakash15 <prakashkumarsingh483@gmail.com>
2022-06-23 02:38:19 +0530

    Updates

---
+++ precache-manifest.833dd8576d193e1fa80c671c0d85d9f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "229b3f744edb4a436d037e3cd07ecfeb",
+    "url": "/Box-Office/index.html"
+  },
+  {
+    "revision": "8c328771db92e4dfe2c9",
+    "url": "/Box-Office/static/css/main.1cf6dec2.chunk.css"
+  },
+  {
+    "revision": "4f30b943caf17be5cbdc",
+    "url": "/Box-Office/static/js/2.afb128b1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Box-Office/static/js/2.afb128b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c328771db92e4dfe2c9",
+    "url": "/Box-Office/static/js/main.983c22ce.chunk.js"
+  },
+  {
+    "revision": "6740a4f854f98d4fa166",
+    "url": "/Box-Office/static/js/runtime-main.140a3920.js"
+  }
+]);
