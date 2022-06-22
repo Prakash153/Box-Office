ae7a4f4380864c9b6ef51ba65807f9966b82200a
Prakash15 <prakashkumarsingh483@gmail.com>
2022-06-23 02:23:41 +0530

    added animation to cards using react fade in

--- src/components/styled.jsx
+++ src/components/styled.jsx
@@ -1,20 +1,21 @@
 import styled from 'styled-components';
+import FadeIn from 'react-fade-in/lib/FadeIn';
 
-export const FlexGrid = styled.div`
+export const FlexGrid = styled(FadeIn)`
   display: flex;
   justify-content: center;
   flex-wrap: wrap;
 `;
 
 export const SearchCard = styled.div`
-  width: 300px;
+  width: 200px;
   height: 100%;
   margin: 0 15px 40px;
 
   .img-wrapper {
     width: 100%;
     border-radius: 40px;
-    height: 420px;
+    height: 250px;
     overflow: hidden;
     border: 1px solid #ddd;
 
