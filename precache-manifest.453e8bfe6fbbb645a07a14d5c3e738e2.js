3c4eaa40b40a4d4723bc4ebaf2cfb86d035d002c
Prakash15 <prakashkumarsingh483@gmail.com>
2022-06-23 03:01:46 +0530

    Updates

---
+++ precache-manifest.453e8bfe6fbbb645a07a14d5c3e738e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f6e8790482c767c3fc6b26bc2dd6286",
+    "url": "/Box-office/index.html"
+  },
+  {
+    "revision": "55bafed6681538bf51db",
+    "url": "/Box-office/static/css/main.1cf6dec2.chunk.css"
+  },
+  {
+    "revision": "ff5e95cc440334e22ee7",
+    "url": "/Box-office/static/js/2.35a5f577.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Box-office/static/js/2.35a5f577.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55bafed6681538bf51db",
+    "url": "/Box-office/static/js/main.b7177886.chunk.js"
+  },
+  {
+    "revision": "04694f4ae56ba9591ca9",
+    "url": "/Box-office/static/js/runtime-main.b4350790.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Box-Office/precache-manifest.63b502100a8b599144f9391a7c8b32c0.js"
+  "/Box-office/precache-manifest.453e8bfe6fbbb645a07a14d5c3e738e2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Box-Office/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Box-office/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
